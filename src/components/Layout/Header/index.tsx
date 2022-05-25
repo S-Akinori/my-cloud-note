e435fd84888059ab64d897068560437e56e0c3bb
akinori <aki10.working@gmail.com>
2022-05-25 21:30:28 +0900

    fix: add deploy hook

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,3 +1,5 @@
+import { LoadingButton } from '@mui/lab';
+import { Button } from '@mui/material';
 import Link from 'next/link';
 import React, { createContext, useState } from 'react';
 
@@ -9,14 +11,28 @@ interface ContextValue {
 
 const Header = () => {
   const [open, setOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [btnText, setBtnText] = useState('同期')
   const value = {
     open,
     setOpen
   }
 
-  const toggleOpen = (toggle: boolean) => {
-    setOpen(toggle);
+  const syncNote = async () => {
+    if(process.env.NEXT_PUBLIC_DEPLOY_HOOK_URL) {
+      setLoading(true)
+      const res = await fetch(process.env.NEXT_PUBLIC_DEPLOY_HOOK_URL, {
+        method: 'POST'
+      })
+      if(res.ok) {
+        setBtnText('同期しました')
+        setTimeout(() => {
+          setBtnText('同期')
+        }, 1000)
+      }
+    }
   }
+
 
   return (
     <>
@@ -32,6 +48,7 @@ const Header = () => {
       <header className="flex items-center fixed top-0 z-40 px-4 bg-base border-b border-main w-full h-16">
         <div className="container flex justify-between items-center mx-auto">
           <div><Link href='/'>My NoteBook</Link></div>
+          <div><LoadingButton onClick={syncNote} loading={loading}>{btnText}</LoadingButton></div>
         </div>
       </header>
       <div className="h-16"></div>
